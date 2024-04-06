24b3eb418c9da7d1db1f0432983d9071496350f3
Rodika Chorba <158561902+RodikaChorba@users.noreply.github.com>
2024-04-06 20:54:51 +0200

    change Number.parseFloat

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,9 @@
 
 function getElementWidth(content, padding, border) {
     // Remove "px" from the strings and convert to integers
-    const contentWidth = parseInt(content);
-    const paddingWidth = parseInt(padding);
-    const borderWidth = parseInt(border);
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding);
+    const borderWidth = Number.parseFloat(border);
     // Calculate the total width (including padding and border)
     const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
 
